1e9796e930d12e35a2d436d6c7802e582642e9ac
zvasileva <zojka-homka@rambler.ru>
2020-02-09 23:23:50 +0300

    Выполнено первое задание

---
+++ 1.1-variables/task.js
@@ -0,0 +1,21 @@
+function averageMark(a, g, p) {
+    let algebra = a;
+    let geography = g;
+    let physics = p;
+    let averageValue = (a + g + p) / 3;
+    return averageValue;
+}
+
+function sayHello(userName) {
+    let myName = userName;
+    let message = `Привет, мир! Меня зовут ${myName}`
+    return message;
+}
+
+function calculateFormula() {
+    let x = 2;
+    let y = 22;
+    let z = 0;
+    let result = x * y + 5 * z + x - 1;
+    return result;
+}
