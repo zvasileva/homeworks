80c49350b81f7705435a2b038d9866a76e1e3bab
zvasileva <zojka-homka@rambler.ru>
2020-02-26 01:03:25 +0300

    Добавлено решение задания 2.1

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -3,19 +3,16 @@
 function getResult(a,b,c){
     let d = b**2 - 4*a*c;
     let x = [];
-    if (d < 0) {
-      return x;
-    } else if (d > 0) {
-      let x1 = (-(b) + Math.sqrt(d)) / (2 * a);
-      let x2 = (-(b) - Math.sqrt(d)) / (2 * a);
-      x.push(x1);
-      x.push(x2);
-      return x;
-    } else if (d = 0) {
+    if (d > 0) {
+        let x1 = (-(b) + Math.sqrt(d)) / (2 * a);
+        let x2 = (-(b) - Math.sqrt(d)) / (2 * a);
+        x.push(x1);
+        x.push(x2);
+    } else if (d === 0) {
         let x1 = (-(b) + Math.sqrt(d)) / (2 * a);
         x.push(x1);
-        return x;
     }
+    return x;
 }
 
 function getAverageMark(marks){
