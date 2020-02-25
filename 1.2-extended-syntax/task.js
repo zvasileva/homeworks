dcbed215b3de080496700f160da33e27bfa97da3
zvasileva <zojka-homka@rambler.ru>
2020-02-26 00:36:52 +0300

    Добавлено решение задания 2.1

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,8 +1,21 @@
 "use strict";
 
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    let d = b**2 - 4*a*c;
+    let x = [];
+    if (d < 0) {
+      return x;
+    } else if (d > 0) {
+      let x1 = (-(b) + Math.sqrt(d)) / (2 * a);
+      let x2 = (-(b) - Math.sqrt(d)) / (2 * a);
+      x.push(x1);
+      x.push(x2);
+      return x;
+    } else if (d = 0) {
+        let x1 = (-(b) + Math.sqrt(d)) / (2 * a);
+        x.push(x1);
+        return x;
+    }
 }
 
 function getAverageMark(marks){
