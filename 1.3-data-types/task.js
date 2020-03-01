dde002bcd2d266a9a95a22ba7b4c98123b2ef91d
zvasileva <zojka-homka@rambler.ru>
2020-03-01 17:54:09 +0300

    Добавлено решение задания 1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,33 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    if (isNaN(percent)) {
+      return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
+    }
+
+    if (isNaN(contribution)) {
+      return `Параметр "Сумма первоначального взноса" содержит неправильное значение ${contribution}`;
+    }
+
+    if (isNaN(amount)) {
+      return `Параметр "Сумма кредита" содержит неправильное значение ${amount}`;
+    }
+
+    let p = percent/12/100;
+    let creditBody = amount - contribution;
+    let now = new Date();
+    let n = Math.floor(Math.abs(now.getTime() - date.getTime()) / (1000 * 3600 * 24)/30);
+    let monthAmount = creditBody * (p + p/(((1 + p)**n)-1));
+    let totalAmount = monthAmount * n;
+    totalAmount = totalAmount.toFixed(2);
+    console.log(totalAmount);
+    return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    if (name.replace(/\s/g, '') === "") {
+        name = 'Аноним';
+     }
+    let greeting = `Привет, мир! Меня зовут ${name}`;
+    return greeting;
 }
