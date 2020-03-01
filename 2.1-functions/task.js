c31fce95affbc25554460457d630501ee3d67538
zvasileva <zojka-homka@rambler.ru>
2020-03-01 15:39:33 +0300

    Добавлено решение третьей задачи задания 2.1

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -66,3 +66,20 @@ function getAverageScore(data) {
 
     return result;
 }
+
+function getDecodedValue(secret) {
+  let result;
+  if (secret === 0) {
+    result = 'Родриго';
+  } else if (secret === 1) {
+    result = 'Эмильо';
+  }
+  return result;
+}
+
+function getPersonData(secretData) {
+  let firstName = getDecodedValue(secretData.aaa);
+  let lastName = getDecodedValue(secretData.bbb);
+  let result = {firstName, lastName};
+  return result;
+}
